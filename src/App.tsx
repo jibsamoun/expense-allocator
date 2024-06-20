f068e4804ba4043f462b7e78587069e049d88925
jibsamoun <jibrilsamoun@gmail.com>
2024-06-20 13:18:22 -0700

    Increased the distance between Username and Password input boxes in LandingPage.tsx by 8px. Created the frontend of SignUpPage.tsx for new users to create their accounts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import SignUpPage from "./pages/SignUpPage";
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
     </Routes>
   );
-}
+};
 
 export default App;
